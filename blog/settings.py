2f10cb720494101969651ab1486cdea2bec32b57
milankathiriya <milankathiriya99@gmail.com>
2018-09-11 13:50:30 +0530

    add 'milanblog.herokuapp.com' into allowed host

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x!^k=88$$h55sym0)2j&8iunf_es+b38&=0a-qu7=!3pp($ruw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milanblog.herokuapp.com']
 
 
 # Application definition
