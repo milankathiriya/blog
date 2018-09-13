7fe98903a1372aa435da9acd90d5a7b8d00f3b20
milankathiriya <milankathiriya99@gmail.com>
2018-09-13 11:08:04 +0530

    image resized logic added

--- post/models.py
+++ post/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -14,3 +15,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title[:30] + '...'
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.width > 1200 or img.height > 700:
+            op_size = (1200, 700)
+            img.thumbnail(op_size)
+            img.save(self.image.path)
